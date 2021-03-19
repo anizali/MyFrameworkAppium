fd6cdc157d9efccd52138e78551e1a9669a9b6c7
anizali <65526871+anizali@users.noreply.github.com>
2021-03-19 15:49:03 +1100

    android app launch commit

--- src/test/java/com/qa/stepdef/Hooks.java
+++ src/test/java/com/qa/stepdef/Hooks.java
@@ -1,13 +1,34 @@
 package com.qa.stepdef;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Hooks {
 
+
+
     @Before
-    public void initialise() {
+    public void initialise() throws MalformedURLException {
         System.out.println("Initialise");
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy S10+");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        caps.setCapability(MobileCapabilityType.UDID, "R58M388CESN");
+        caps.setCapability(MobileCapabilityType.APPLICATION_NAME, "Android");
+        //caps.setCapability(MobileCapabilityType., "Android");
+
+        URL url = new URL("http://0.0.0.0:4723/wd/hub");
+
+        AppiumDriver driver = new AndroidDriver(url, caps);
 
 
     }
--- src/test/resources/Calender.feature
+++ src/test/resources/Calender.feature
@@ -1,7 +1,9 @@
-Feature: Calender Meeting
+Feature: Calender
 
-  Scenario Outline: Create a workshop meeting event from 9:30am to 1-30pm
+  Scenario Outline: Create a workshop meeting event from nine to one
     Given I have launched the calendar App
     When It is a working Friday
     Examples:
+      | Platform |
+      | Android  |
 
